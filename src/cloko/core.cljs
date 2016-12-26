f7e0dd31e0b371f3ce54b4eb30fd5fff79c520d8
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2016-12-26 16:07:01 +0100

    Add docstrings to functions

--- src/cloko/core.cljs
+++ src/cloko/core.cljs
@@ -23,10 +23,14 @@
 (defn- place-planets [world planets]
   (reduce #(assoc-in %1 (reverse (:position %2)) (:owner %2)) world planets))
 
-(defn- world-as-dots [[x y]]
+(defn- world-as-dots
+  "Returns a 2d vector with the dimensions provided by [x y]."
+  [[x y]]
   (vec (repeat y (vec (repeat x :.)))))
 
-(defn show-board! []
+(defn show-board!
+  "Prints the current world. Dots represent empty fields.
+  Numbers represent planets of the corresonding player. 'N' belongs to the neutral player"[]
   (let [world (get @game-state :world)
         planets (get world :planets)
         dims (get world :size)]
@@ -37,9 +41,12 @@
          (replace {:. "." :player1 "1" :player2 "2" :neutral "N"})
          (print))))
 
-(defn players-turn? [player] (= player (whose-turn)))
+(defn players-turn?
+  "Returns true if it is players turn."
+  [player]
+  (= player (whose-turn)))
 
 (defn update-planet
-  "Updates the ship amount on this planet by adding the ship generation factor"
+  "Updates the ship amount on this planet by adding the ship generation factor."
   [planet]
   (update-in planet [:ships] + (:ships-per-turn planet)))
