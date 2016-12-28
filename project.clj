c44bba670ad9eb11881e7798b114e421b95c9e3e
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2016-12-28 14:08:43 +0100

    Update cljs

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject cloko "0.0.1"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]]
+                 [org.clojure/clojurescript "1.9.293"]]
   :plugins [[lein-figwheel "0.5.8"]]
+  :profiles {:test {:dependencies [[lein-kibit "0.1.3"]]}}
   :clean-targets [:target-path "out"]
   :cljsbuild {
               :builds [{:id "dev"
