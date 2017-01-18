798ad19c7a351be25613fdb009d90eb30eb05bf0
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-01-18 15:21:42 +0100

    Fix tests

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject cloko "0.1.0"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.293"]
                  [org.clojure/test.check "0.9.0"]
@@ -10,19 +12,24 @@
             [devcards "0.2.2"]
             [lein-cljsbuild "1.1.5" :exclusions [[org.clojure/clojure]]]
             [lein-ancient "0.6.10"]]
-  ;:hooks [leiningen.cljsbuild]
-  :target-path "target/%s"
+  :hooks [leiningen.cljsbuild]
+  :figwheel {
+             :css-dirs ["resources/public/css"]}
   :doo {
         :build "test"
         :alias {:default [:phantom]}
         :paths {:phantom "phantomjs --web-security=false"}}
 
   :profiles {:kibit {:plugins [[lein-kibit "0.1.3"]]}}
-  :clean-targets ^{:protect false} [:target-path "resources/public/cljs" "out"]
+  :clean-targets ^{:protect false} [:target-path "resources/public/cljs/*" "out"]
   :cljsbuild {
+              :test-commands {"test" ["lein" "doo"]}
               :builds {:dev {:source-paths ["src"]
                              :figwheel true
                              :compiler {:main "cloko.frontend"
+                                        :optimizations :none
+                                        :pretty-print true
+                                        :source-map true
                                         :asset-path "cljs/dev/out"
                                         :output-dir "resources/public/cljs/dev/out"
                                         :output-to "resources/public/cljs/dev/main.js"}}
@@ -30,7 +37,7 @@
                               :source-paths ["src" "test"]
                               :compiler {:main runners.doo
                                          :optimizations :none
-                                         :asset-path "cljs/test/out"
+                                         :asset-path "resources/public/cljs/test/out"
                                          :output-dir "resources/public/cljs/test/out"
                                          :output-to "resources/public/cljs/test/all-tests.js"}}
                        :devcard-test {
@@ -39,6 +46,14 @@
                                       :compiler {:main runners.browser
                                                  :optimizations :none
                                                  :asset-path "cljs/devcard-test/out"
-                                                 :output-to "resources/public/cljs/devcard-test/all-tests.js"
                                                  :output-dir "resources/public/cljs/devcard-test/out"
-                                                 :source-map-timestamp true}}}})
+                                                 :output-to "resources/public/cljs/devcard-test/all-tests.js"
+                                                 :source-map-timestamp true}}
+                       :min {
+                             :source-paths ["src"]
+                             :compiler {
+                                        :optimizations :advanced
+                                        :elide-asserts true
+                                        :output-to "resources/public/cljs/min/main.js.min"}}}})
+
+
