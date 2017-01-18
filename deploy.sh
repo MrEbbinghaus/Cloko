c83642ba387a9960bbecaee970a92540157a17cc
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-01-18 17:23:19 +0100

    Update deployment to clone single branch

--- deploy.sh
+++ deploy.sh
@@ -22,10 +22,8 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
+git clone $REPO --branch $TARGET_BRANCH --single-branch out
 cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
 
 # Clean out existing contents
 git rm -rf out/**/* || exit 0
