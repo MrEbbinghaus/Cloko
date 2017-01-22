dd2e7f4c093f2d407f77b3d6ed33b22d2a9d05df
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-01-22 12:13:00 +0100

    Add whose turn badge

--- src/cloko/frontend.cljs
+++ src/cloko/frontend.cljs
@@ -4,6 +4,9 @@
 
 (def planet-icon [:img {:src "assets/planets/europa.svg"}])
 (def nplanet-icon [:img {:src "assets/planets/mars.svg"}])
+(def next-turn-btn [:button.btn {:on-click core/end-turn!} "Next turn"])
+
+(defn whose-turn [state] [:span.badge (name (get (:players state) (:whosTurn state)))])
 
 (defn field
   [planet]
@@ -16,20 +19,17 @@
   [x y-size planets]
   (mapv #(field (get-in planets [[% x] :owner] nil)) (range y-size)))
 
-(defn board []
-  (let [state @core/game-state
-        [x-size y-size] (get-in state [:world :size])
+(defn board [state]
+  (let [[x-size y-size] (get-in state [:world :size])
         planets (get-in state [:world :planets])]
     (vec (apply concat [:div.board] (mapv #(row % y-size planets) (range x-size))))))
-
-(defn movement-info []
-  (let [state @core/game-state]))
 
 (defn app []
   (let [state @core/game-state]
     [:div
-     [:div (board)]
-     [:div (movement-info)]]))
+     [:div (whose-turn state)]
+     (board state)
+     [:div next-turn-btn]]))
 
 (defn ^:export main []  ;; do not forget the export
   (core/init! 9 9 3 2)
