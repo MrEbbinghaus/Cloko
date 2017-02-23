e70acbaf7da7e8518581e356fc9db4a4706d00c6
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-02-23 12:01:07 +0100

    Add placeholder for movement info

---
+++ src/cloko/components/movement_info.cljs
@@ -0,0 +1,10 @@
+(ns cloko.components.movement-info
+  (:require [reagent.core :as r]
+    [cloko.core :as core]))
+
+(defn movement-info-panel
+  [state]
+  (let []
+    [:div.panel.panel-default
+     [:div.panel-heading "Movement information"]
+     [:div.panel-body "Bla"]]))
