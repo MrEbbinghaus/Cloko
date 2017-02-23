51214314daf5d729779dffed50d0ce6501cd2acf
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-02-23 15:21:49 +0100

    Implement movement info panel
    Closes: #12

--- src/cloko/components/movement_info.cljs
+++ src/cloko/components/movement_info.cljs
@@ -2,9 +2,22 @@
   (:require [reagent.core :as r]
     [cloko.core :as core]))
 
+(defn display-movement [movement]
+  [:table.table.table-condensed
+   [:tbody
+    [:tr [:td "From: "] [:td (str (:origin movement))]]
+    [:tr [:td "To: "] [:td (str (:target movement))]]
+    [:tr [:td "Ships: "] [:td (:ships movement)]]
+    [:tr [:td "Rounds until arrival: "] [:td (:rounds-until-arrival movement)]]]])
+
 (defn movement-info-panel
   [state]
-  (let []
+  (let [movements (core/player-movements state)]
     [:div.panel.panel-default
      [:div.panel-heading "Movement information"]
-     [:div.panel-body "Bla"]]))
+     [:div.panel-body
+      (if (empty? movements)
+        [:p "No movements!"]
+        [:ul.list-group
+         (for [movement movements]
+           ^{:key movement} [:li.list-group-item (display-movement movement)])])]]))
