efa75acbb51c4f02e0c2cc6f87a4f5ed512943f2
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-02-26 20:32:54 +0100

    Hide stationed ships on planets you don't own

--- src/cloko/components/planet_info.cljs
+++ src/cloko/components/planet_info.cljs
@@ -2,20 +2,22 @@
   (:require [reagent.core :as r]
             [cloko.core :as core]))
 
-(defn planet-details [planet [x y]]
+(defn planet-details [planet [x y] current-player]
   [:table.table
    [:tbody
     [:tr [:td "Position"] [:td x ":" y]]
     [:tr [:td "Owner"] [:td (:owner planet)]]
     [:tr [:td "Ships per turn"] [:td (:ships-per-turn planet)]]
-    [:tr [:td "Stationed ships"] [:td (:ships planet)]]]])
+    (when (= current-player (:owner planet))
+      [:tr [:td "Stationed ships"] [:td (:ships planet)]])]])
 
 (defn planet-info-panel
   [state]
   (let [planet-position (get-in state [:fe :selected-planet])
-        planet (get-in state [:world :planets planet-position])]
+        planet (get-in state [:world :planets planet-position])
+        current-player (core/whose-turn state)]
     [:div.panel.panel-default
      [:div.panel-heading "Planet information"]
      [:div.planet-info.panel-body (if planet
-                                    (planet-details planet planet-position)
+                                    (planet-details planet planet-position current-player)
                                     [:p "No planet selected!"])]]))
