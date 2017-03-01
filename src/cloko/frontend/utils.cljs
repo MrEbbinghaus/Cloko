8f85d3968fafc30652e3a534dd037296e87ff2d3
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-03-01 18:34:24 +0100

    Add documentation for the util namespace

--- src/cloko/frontend/utils.cljs
+++ src/cloko/frontend/utils.cljs
@@ -1,8 +1,12 @@
 (ns cloko.frontend.utils
+  "Contains utilities for the frontend."
   (:require [reagent.core :as r]
             [clojure.string :refer [join]]))
 
+;; inter-component state
 (defonce fe-state (r/atom {}))
 
-(defn position-formater [v] (join ":" v))
+(defn position-formater
+  "Gets a vector v and returns a string in the formatation of the game"
+  [v] (join ":" v))
 
