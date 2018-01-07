55828d1f612c882adf450b7924cdd83a7bdd05db
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2018-01-07 21:03:55 +0100

    Use SVG to draw game board instead of plain HTML

--- src/cloko/frontend/components/board.cljs
+++ src/cloko/frontend/components/board.cljs
@@ -3,41 +3,37 @@
     [cloko.core :refer [game-state]]
     [cloko.frontend.utils :refer [fe-state]]))
 
-(def planet-icon [:img {:src "assets/planets/europa.svg"}])
-(def nplanet-icon [:img {:src "assets/planets/mars.svg"}])
+
+(def player-colors {:neutral "white"
+                    :player1 "red"
+                    :player2 "green"
+                    :player3 "blue"})
+(def planet-icon [:image {:xlink:href "assets/planets/europa.svg"}])
+(def nplanet-icon [:image {:xlink:href "assets/planets/mars.svg"}])
 
 (defn set-selected-position!
   "Sets pos as the selected planet in the frontend state."
   [pos]
   (swap! fe-state assoc :selected-planet pos))
 
-(defn field
+(defn planet
   "Returns a react component of a field on the board. Gets two coordinates of the field."
-  [x y]
+  [[[x y] p]]
   (fn []
-    (let [world (:world @game-state)
-          planet (get-in world [:planets [x y]] nil)
-          [width height] (mapv #(/ 100 %) (:size world))]
-      (if planet
-        [:div.field.planet
-         {:style {:width (str width "%")
-                  :height (str height "%")}
-          :data-x x :data-y y
-          :on-click #(set-selected-position! [x y])}
-         (if (= :neutral (:owner planet)) nplanet-icon planet-icon)]
-        [:div.field.space {:style {:width (str width "%")
-                                   :height (str height "%")}}]))))
+    (let [selected? (= (:selected-planet @fe-state) [x y])]
+      [:circle.planet
+       {:class (str (name (:owner p)) " " (when selected? "selected")) ; theres a bug in reagent
+        :cx x :cy y
+        :r 0.4
+        :data-x x :data-y y
+        :on-click #(set-selected-position! [x y])}])))
 
 (defn board []
   "Return a react component of the board."
   (fn []
     (let [world (:world @game-state)
-          [x-size y-size] (:size world)
-          planets (:planets world)]
-      [:div.outer-board
-       [:div.inner-board
-        {:style {:width (* 55 x-size)
-                 :height (* 55 y-size)}}
-
-        (for [x (range x-size) y (range y-size)]
-          ^{:key [x y]} [field x y])]])))
+          [x-size y-size] (:size world)]
+      [:div.board-wrapper
+       [:svg.board
+        {:viewBox [-1 -1 (inc x-size) (inc y-size)]}
+        (map (comp vector planet) (:planets world))]])))
