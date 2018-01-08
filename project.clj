08666f071f3c440fccf192064ea65d25216be103
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2018-01-08 01:14:19 +0100

    Bump version: 1.1.0 → 1.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cloko "1.1.0"
+(defproject cloko "1.2.0"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
